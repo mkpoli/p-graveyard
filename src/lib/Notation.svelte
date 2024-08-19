ddafdd21caa1a2574942eaefbd69c91d38301437
mkpoli <git@mkpo.li>
2024-08-20 00:53:37 +0900

    feat: add phonological Notation component

---
+++ src/lib/Notation.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	let {
+		changes,
+		condition = undefined
+	}: {
+		changes: string[];
+		condition?: string;
+	} = $props();
+</script>
+
+<span>
+	{#each changes as change}
+		<code class="form">{change}</code>
+	{/each}
+</span>
+
+{#if condition}
+	<code class="condition">{condition}</code>
+{/if}
+
+<style>
+	code.form:not(:last-child)::after {
+		content: ' > ';
+	}
+
+	code.condition::before {
+		content: ' / ';
+	}
+</style>
